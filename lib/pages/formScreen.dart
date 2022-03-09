91cf5eef3f36b020b7a49181d5db1d11b3c0279b
Rodrigo Tellez <rodrigote.ti20@utsjr.edu.mx>
2022-03-09 14:37:52 -0600

    9 de marzo

--- lib/pages/formScreen.dart
+++ lib/pages/formScreen.dart
@@ -33,14 +33,135 @@ class _MyStateFulWidgetState extends State<MyStateFulWidget> {
     return Scaffold(
       body: Center(
         
-        child: Padding(
-          padding: const EdgeInsets.all(50.0),
+        child: Form(
+          
           child: Column(
-            //padding: const EdgeInsets.all(40.0),
             mainAxisAlignment: MainAxisAlignment.center,
             children:<Widget>[
-              //como poner paddind
-              TextField(
+             inputEmail(_controller),
+             const SizedBox(height: 10),
+             inputPassword(),
+             const SizedBox(height: 10),
+             inputCard(),
+             const SizedBox(height: 10),
+             Container(
+               
+               padding: const EdgeInsets.symmetric(horizontal: 20.0), //interno
+                margin: const EdgeInsets.symmetric(horizontal: 30.0),
+               child: CheckboxListTile(
+                 
+                 title: const Text('Validar'),
+                 
+                 value: _isChecked,
+                onChanged: (bool? value) {
+                      setState(() { //cambia el estado de la viriable
+                        _isChecked = value!;
+                      });
+                     },
+                ),
+             ),
+
+            
+
+             /*CheckboxListTile(
+                  controlAffinity: ListTileControlAffinity.leading,
+                  title: const Text('Primero'),
+                  value: _isChecked, 
+                  onChanged: (bool? value) {
+                    setState(() { //cambia el estado de la viriable
+                      _isChecked = value!;
+                    });
+                   },
+              
+             ),*/
+             const SizedBox(height: 10),
+             ElevatedButton(
+               
+               onPressed: ()=>{},
+               child: const Text('Enviar'))
+            ], 
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+
+
+Widget inputCard() {
+  return  Container(
+    padding: const EdgeInsets.symmetric(horizontal: 20.0), //interno
+   margin: const EdgeInsets.symmetric(horizontal: 30.0), //externo
+   decoration: BoxDecoration(
+    borderRadius: BorderRadius.circular(20),
+    border: Border.all(color: Colors.blue, width: 3.0)
+    ),
+    child: TextField(
+                   keyboardType: TextInputType.number, //numeros en el teclado
+                   inputFormatters: [
+                     FilteringTextInputFormatter.allow(
+                       RegExp('[0-9 -]')
+                     ),
+                     LengthLimitingTextInputFormatter(16)
+                   ], //reglas
+                   decoration: const InputDecoration(
+                    prefixIcon: Icon(Icons.credit_card),
+                    labelText: 'Tarjeta',
+                    hintText: '1234-1234-1234-1234'
+                   ),
+                ),
+  );
+}
+
+Widget inputPassword() {
+  return  Container(
+    padding: const EdgeInsets.symmetric(horizontal: 20.0), //interno
+  margin: const EdgeInsets.symmetric(horizontal: 30.0), //externo
+  decoration: BoxDecoration(
+    borderRadius: BorderRadius.circular(20),
+    border: Border.all(color: Colors.blue, width: 3.0)
+    ),
+    child: TextFormField(
+                  //controller: _controller,
+                  obscureText: true,
+                   decoration: InputDecoration(
+                    prefixIcon: Icon(Icons.lock),
+                    labelText: 'Contraseña',
+                   ),
+                ),
+  );
+}
+
+
+//METODO PARA EL FORMULARIO
+Widget inputEmail(_controller){
+  return Container( //permite agregar unas decoraciones
+  padding: const EdgeInsets.symmetric(horizontal: 20.0), //interno
+  margin: const EdgeInsets.symmetric(horizontal: 30.0), //externo
+  decoration: BoxDecoration(
+    borderRadius: BorderRadius.circular(20),
+    border: Border.all(color: Colors.blue, width: 3.0)
+    ),
+
+    child: TextFormField(
+       keyboardType: TextInputType.emailAddress,
+       autofocus: true,
+       controller: _controller,
+        decoration: const InputDecoration(
+        border: InputBorder.none,
+        prefixIcon: Icon(Icons.contact_mail),
+        labelText: 'Correo',
+        hintText: 'micorreo@correo.com'
+        ),
+
+      ),
+  );
+}
+
+/*
+
+TextFormField(
                 keyboardType: TextInputType.emailAddress,
                 autofocus: true,
                 controller: _controller,
@@ -54,6 +175,7 @@ class _MyStateFulWidgetState extends State<MyStateFulWidget> {
               ),
               const SizedBox(height: 20.0),
               const TextField(
+                //controller: _controller,
                 obscureText: true,
                  decoration: InputDecoration(
                   prefixIcon: Icon(Icons.lock),
@@ -113,14 +235,7 @@ class _MyStateFulWidgetState extends State<MyStateFulWidget> {
 
                 )    
 
-            ], 
-          ),
-        ),
-        ),
-    );
-  }
-}
-
+*/
 /*
  onSubmitted: (String value ) async {
               await showDialog<void>(
